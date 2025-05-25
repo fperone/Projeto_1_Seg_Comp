809703a53763015a1bf68235dbbed3cb0d1f9b38
fperone <148684728+fperone@users.noreply.github.com>
2025-05-25 15:59:05 -0300

    Update Decifrador.py
    Decifrador de cifras de Vinigere

--- Decifrador.py
+++ Decifrador.py
@@ -1,51 +1,49 @@
-abc = ["A","B","C","D","E","F", "G", "H", "I", "J", "K", "L", "M", "N", "O", 
-       "P", "Q", "R", "S", "T", "U", "V", "W", "X","Y","Z"]
+def Decifrador(criptograma, key):
+       abc = ["A","B","C","D","E","F", "G", "H", "I", "J", "K", "L",
+              "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X","Y","Z"]
 
-acentos = {'Á': 'A', 'À': 'A', 'Ã': 'A', 'Â': 'A', 'Ä': 'A',
-           'É': 'E', 'È': 'E', 'Ê': 'E', 'Ë': 'E',
-           'Í': 'I', 'Ì': 'I', 'Î': 'I', 'Ï': 'I',
-           'Ó': 'O', 'Ò': 'O', 'Õ': 'O', 'Ô': 'O', 'Ö': 'O',
-           'Ú': 'U', 'Ù': 'U', 'Û': 'U', 'Ü': 'U',
-           'Ç': 'C'}
+       acentos = {'Á': 'A', 'À': 'A', 'Ã': 'A', 'Â': 'A', 'Ä': 'A',
+                  'É': 'E', 'È': 'E', 'Ê': 'E', 'Ë': 'E',
+                  'Í': 'I', 'Ì': 'I', 'Î': 'I', 'Ï': 'I',
+                  'Ó': 'O', 'Ò': 'O', 'Õ': 'O', 'Ô': 'O', 'Ö': 'O',
+                  'Ú': 'U', 'Ù': 'U', 'Û': 'U', 'Ü': 'U',
+                  'Ç': 'C'}
 
-def limpar_texto(texto):
-    texto_novo = ''
-    for c in texto.upper():
-        if c.isalpha() or c == ' ':
-            if c in acentos:
-                texto_novo += acentos[c]
-            else:
-                texto_novo += c
-    return texto_novo
+       def limpar_texto(texto):
+           texto_novo = ''
+           for c in texto.upper():
+               if c in acentos:
+                   texto_novo += acentos[c]
+               elif c.isalpha():
+                   texto_novo += c
+           return texto_novo
 
-criptograma = input("Digite o criptograma: ").upper()
-key = input("Digite a chave: ").upper()
 
-# Verifica se há números
-for ele in criptograma:
-    if ele.isdigit():
-        print("O texto não pode conter números")
-        exit()
-for ele in key:
-    if ele.isdigit():
-        print("A chave não pode conter números")
-        exit()
+       # Limpa chave (só letras, sem espaços)
+       key = limpar_texto(key)
 
-# Limpa chave
-key = limpar_texto(key).replace(" ", "")  # Retira espaços da chave
+       # Processo de decifra
+       mensagem = ""
+       key_index = 0
 
-mensagem = ""
-k = 0  # contador da chave
+       for c in criptograma:
+           c_maiusculo = c.upper()
+       
+           # Se for letra sem acento
+           if c_maiusculo in abc:
+               n = abc.index(c_maiusculo)
+               k = abc.index(key[key_index % len(key)])
+               msg_letter = ((n - k) + 26) % 26
+               letra_decifrada = abc[msg_letter].lower() if c.islower() else abc[msg_letter]
+               mensagem += letra_decifrada
+               key_index += 1
+       
+           # Se for letra com acento, mantém inalterada
+           elif c_maiusculo in acentos.keys():
+               mensagem += c
+       
+           # Se for espaço, vírgula, ponto ou outro caractere especial, mantém inalterado
+           else:
+               mensagem += c
 
-for e in range(len(criptograma)):
-    char = criptograma[e]
-    if char == ' ':
-        mensagem += ' '
-    else:
-        n = abc.index(char)
-        kn = abc.index(key[k % len(key)])
-        msg_letter = ((n - kn) + 26) % 26
-        mensagem += abc[msg_letter]
-        k += 1  # só avança a chave se processou uma letra
-
-print(mensagem)
+       return mensagem
