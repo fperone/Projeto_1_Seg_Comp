cf394d2b76617bc03ea73b43905bf8977e456495
fperone <148684728+fperone@users.noreply.github.com>
2025-05-25 15:53:34 -0300

    Update Cifrador.py
    Cifrador em cifra de vinigere

--- Cifrador.py
+++ Cifrador.py
@@ -1,27 +1,49 @@
-abc = ["A","B","C","D","E","F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X","Y","Z"]
-acentos = {'Á': 'A', 'À': 'A', 'Ã': 'A', 'Â': 'A', 'Ä': 'A','É': 'E', 'È': 'E', 'Ê': 'E', 'Ë': 'E','Í': 'I', 'Ì': 'I', 'Î': 'I', 'Ï': 'I','Ó': 'O', 'Ò': 'O', 'Õ': 'O', 'Ô': 'O', 'Ö': 'O','Ú': 'U', 'Ù': 'U', 'Û': 'U', 'Ü': 'U','Ç': 'C'}
-def limpar_texto(texto):
-  texto_novo = ''.join([c.upper() for c in texto if c.isalpha()])
-  for i in texto_novo:
-    if i in acentos:
-      texto_novo = texto_novo.replace(i, acentos[i])
-  return texto_novo
-texto, key = input("Digite a mensagem: "), input("Digite a chave: ")
-for ele in texto:
-  if ele.isdigit():
-    print("O texto não pode conter números")
-    exit()
-for ele in key:
-  if ele.isdigit():
-    print("A chave não pode conter números")
-    exit()
-key = limpar_texto(key)
-texto = limpar_texto(texto)
-criptograma = ""
-for e in range(len(texto)):
-  n = abc.index(texto[e])
-  k = e % len(key)
-  kn = abc.index(key[k])
-  new_letter = (n + kn) % 26
-  criptograma += abc[new_letter]
-print(criptograma)
+def Cifrar(texto, key)
+  abc = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
+       "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+  acentos = {'Á': 'A', 'À': 'A', 'Ã': 'A', 'Â': 'A', 'Ä': 'A',
+           'É': 'E', 'È': 'E', 'Ê': 'E', 'Ë': 'E',
+           'Í': 'I', 'Ì': 'I', 'Î': 'I', 'Ï': 'I',
+           'Ó': 'O', 'Ò': 'O', 'Õ': 'O', 'Ô': 'O', 'Ö': 'O',
+           'Ú': 'U', 'Ù': 'U', 'Û': 'U', 'Ü': 'U',
+           'Ç': 'C'}
+
+  def limpar_texto(texto):
+      texto_novo = ''
+      for c in texto.upper():
+          if c in acentos:
+              texto_novo += acentos[c]
+          elif c.isalpha():
+              texto_novo += c
+      return texto_novo
+
+  # Limpa chave (a chave deve ser só letras)
+  key = limpar_texto(key)
+
+  # Processo de cifra
+  criptograma = ""
+  key_index = 0
+
+  for c in texto:
+      c_maiusculo = c.upper()
+  
+      # Verifica se a letra é do alfabeto sem acento
+      if c_maiusculo in abc:
+          n = abc.index(c_maiusculo)
+          k = abc.index(key[key_index % len(key)])
+          new_letter = (n + k) % 26
+          # Mantém minúsculo se a letra original era minúscula
+          letra_cifrada = abc[new_letter].lower() if c.islower() else abc[new_letter]
+          criptograma += letra_cifrada
+          key_index += 1
+  
+      # Se for letra com acento, mantém inalterada
+      elif c_maiusculo in acentos.keys():
+          criptograma += c
+  
+      # Se for espaço, vírgula, ponto ou outro caractere especial, mantém inalterado
+      else:
+          criptograma += c
+
+  return criptograma
