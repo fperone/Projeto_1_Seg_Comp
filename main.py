adbe80998284580259695ee0dbfb21458457c123
fperone <148684728+fperone@users.noreply.github.com>
2025-05-25 16:40:19 -0300

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ else:
 
 
 #Função Cifrar
-def Cifrar(texto, key)
+def Cifrar(texto, key):
   abc = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
        "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
